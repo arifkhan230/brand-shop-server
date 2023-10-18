b05dee88027b42c5607126c3316d5648244b5c9f
arifkhan101 <103602241+arifkhan101@users.noreply.github.com>
2023-10-18 19:59:50 +0600

    product added to the database and got in server

--- index.js
+++ index.js
@@ -1,8 +1,62 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cwfli1i.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const productCollection = client.db('productDb').collection('products')
+
+    app.get('/products',async(req,res)=>{
+        const result = await productCollection.find().toArray();
+        res.send(result)
+    })
+
+    app.post('/products', async(req,res) =>{
+        const product = req.body;
+        const result = await productCollection.insertOne(product)
+        res.send(result)
+    })
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
 
 app.get('/', (req,res)=>{
     res.send('My brand server is running')
